00e1250b2f562ef1fa5a2bd6c8f8ef01f95447da
MuhammadA-C <phantomganhdi@gmail.com>
2022-08-25 08:27:28 -0400

    Part 1 Computer random choice
    Creating the code for the computer to randomly pick an int that'll be used to pick rock, paper, or scissors.
    
    Created a function that randomly generates an int between 0 to 3 excluding 3, (0,1 or 2).

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+
+
+
+let randomInt = getRandomInt();
+
+
+console.log(randomInt);
+
+function getComputerChoice(){
+
+
+    return 
+}
+
+function getRandomInt(){
+    const max = 3;
+    let result;
+
+    result = Math.floor(Math.random() * max);
+
+    return result;
+}
