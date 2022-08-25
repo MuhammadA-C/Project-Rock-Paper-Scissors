0598a0ca31d849cbf45044e9993419aa8e96b5a4
MuhammadA-C <phantomganhdi@gmail.com>
2022-08-25 09:31:56 -0400

    Game version 1
    Created the first version of the game. This version just plays 1 round of Rock, Paper, Scissors.
    
    Note: I made the results to print the string all in lowercase, in the future I could add a function to make the first string char at index 0 upper case.

--- javascript/script.js
+++ javascript/script.js
@@ -3,25 +3,53 @@
 let computerSelection; 
 let playerSelection;
 let getPlayerSelection;
+let result;
 
 computerSelection = getComputerChoice();
 getPlayerSelection = prompt("Enter Rock, Paper, or Scissors to play!");
 playerSelection = getPlayerSelection.toLowerCase();
+result = playRockPaperScissors(computerSelection, playerSelection);
+
+if (result === "You Lose!"){
+    console.log(result + " " + computerSelection + " beats " + playerSelection);
+}
+else if (result === "You Win!"){
+    console.log(result + " " + playerSelection + " beats " + computerSelection);
+} else {
+    console.log(result);
+}
 
 
 
+
+
+function playRockPaperScissors(computerSelection, playerSelection){
+    let computer = computerSelection.toLowerCase();
+    let player = playerSelection;
+    let result;
+
+    if (computer === player){
+        result = "You Tied!";
+    } 
+    else if (computer === "rock" && player === "scissors" || computer === "scissors" && player === "paper" || computer === "paper" && player === "rock"){
+        result = "You Lose!";
+    } else {
+        result = "You Win!";
+    }
+
+    return result;
+}
+//Gets computers choice
 function getComputerChoice(){
     let randomInt = getRandomInt();
-    let result = getRockPaperOrScissors(randomInt);
+    let result = getRockPaperScissors(randomInt);
 
     return result;
 }
 //Generates a random int between 0 to 3.
 function getRandomInt(){
     const max = 3;
-    let result;
-
-    result = Math.floor(Math.random() * max);
+    let result = Math.floor(Math.random() * max);
 
     return result;
 }
@@ -29,16 +57,16 @@ function getRandomInt(){
     Takes a number argument (num), in this case uses the randomInt funciton,
     and outputs "Rock, Paper, or Scissors" based on the conditional.
 */
-function getRockPaperOrScissors(num){
+function getRockPaperScissors(num){
     let result;
 
     if (num == 0) {
-        result = "Rock";
+        result = "rock";
     } 
     else if (num == 1) {
-        result = "Paper";
+        result = "paper";
     } else {
-        result = "Scissors";
+        result = "scissors";
     }
 
     return result;
